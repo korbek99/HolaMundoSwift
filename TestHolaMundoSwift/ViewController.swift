cd2fb06a24e2fca42c549f5a43158755da3a2965
korbek100 <korbek100@gmail.com>
2018-03-31 10:20:47 -0300

    first commit

---
+++ TestHolaMundoSwift/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TestHolaMundoSwift
+//
+//  Created by Jose David Bustos H on 31-03-18.
+//  Copyright © 2018 Jose David Bustos H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var LabelHola: UILabel!
+    
+    @IBAction func BotonAction(_ sender: Any) {
+        self.LabelHola.text = "Hola Mundo"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func BotonBorrar(_ sender: Any) {
+        self.LabelHola.text = ""
+    }
+}
+
